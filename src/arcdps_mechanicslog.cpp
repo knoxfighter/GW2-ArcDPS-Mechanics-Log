1d110b28d835c8eaefdde8c32c607ace83bbcfe8
LincoHd <47218525+LincoHd@users.noreply.github.com>
2026-05-24 19:36:07 +0200

    Added new line to if and braces

--- src/arcdps_mechanicslog.cpp
+++ src/arcdps_mechanicslog.cpp
@@ -316,7 +316,10 @@ uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t i
 					(current_entry ? current_entry->player : nullptr), //check for null before getting player object
 					(other_entry ? other_entry->player: nullptr)))
 				{
-					if (getMechanics()[index].is_combat_buff && ev->is_statechange == CBTS_BUFFAPPLY) getMechanics()[index].is_combat_buff = false;
+					if (getMechanics()[index].is_combat_buff && ev->is_statechange == CBTS_BUFFAPPLY)
+					{
+						getMechanics()[index].is_combat_buff = false;
+					}
 					tracker.processMechanic(ev, current_entry, other_entry, &getMechanics()[index], value);
 					log_ui.scroll_to_bottom = true;
 				}
